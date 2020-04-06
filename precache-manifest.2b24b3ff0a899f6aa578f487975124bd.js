b10c2e4a646be26d9168d979ea19d11e4a0c6cf5
kim junghwan <kimjunghwan@kimui-MacBookPro.local>
2020-04-06 11:56:26 +0900

    Updates

---
+++ precache-manifest.2b24b3ff0a899f6aa578f487975124bd.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94cadd3d73d0398d1cfec3b045c3c07",
+    "url": "/my-portpolio/index.html"
+  },
+  {
+    "revision": "8b33f75390aff0a668e4",
+    "url": "/my-portpolio/static/css/2.e8186892.chunk.css"
+  },
+  {
+    "revision": "8b33f75390aff0a668e4",
+    "url": "/my-portpolio/static/js/2.eb5d7e76.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/my-portpolio/static/js/2.eb5d7e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd314b3e1906b4eb96c",
+    "url": "/my-portpolio/static/js/main.ee98f6d7.chunk.js"
+  },
+  {
+    "revision": "d888a967b4c4ee4e945b",
+    "url": "/my-portpolio/static/js/runtime-main.85951ece.js"
+  },
+  {
+    "revision": "3e6e88708b8994889779e07c1200ad7f",
+    "url": "/my-portpolio/static/media/1.3e6e8870.png"
+  },
+  {
+    "revision": "3acf86b5cb1d54ba46f32c3bab0c6e00",
+    "url": "/my-portpolio/static/media/2.3acf86b5.png"
+  },
+  {
+    "revision": "f5d7d54b8c594689013531b6ca29842b",
+    "url": "/my-portpolio/static/media/5.f5d7d54b.png"
+  },
+  {
+    "revision": "abe4f55d0a50e910d89e15a5bebd386e",
+    "url": "/my-portpolio/static/media/apache.abe4f55d.png"
+  },
+  {
+    "revision": "0fdb70e59e0463248c8de1bb506b48f9",
+    "url": "/my-portpolio/static/media/home2.0fdb70e5.jpg"
+  },
+  {
+    "revision": "8442a12b4ae5b60511888f8695688ffc",
+    "url": "/my-portpolio/static/media/java.8442a12b.png"
+  },
+  {
+    "revision": "b887b975315bbc6302e400eeb93f3ace",
+    "url": "/my-portpolio/static/media/kurento.b887b975.png"
+  },
+  {
+    "revision": "dc98064e8bc40d83d91362d4ce1dd9d3",
+    "url": "/my-portpolio/static/media/linux.dc98064e.png"
+  },
+  {
+    "revision": "6595445f5265b6db957149da19db77ed",
+    "url": "/my-portpolio/static/media/mariadb.6595445f.png"
+  },
+  {
+    "revision": "8283019916e06ec407805a33ddd66827",
+    "url": "/my-portpolio/static/media/objective-c.82830199.png"
+  },
+  {
+    "revision": "4d3402d11307da1192e297fb362a0551",
+    "url": "/my-portpolio/static/media/postgresql.4d3402d1.png"
+  },
+  {
+    "revision": "45e67e15f78ef4967405c0c356003d75",
+    "url": "/my-portpolio/static/media/python.45e67e15.png"
+  },
+  {
+    "revision": "0a0716368974f3beaef167ced107645d",
+    "url": "/my-portpolio/static/media/redis.0a071636.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/my-portpolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/my-portpolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/my-portpolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/my-portpolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
