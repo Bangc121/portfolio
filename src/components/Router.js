8fd119cc97a42074b5eb3e354a596eb998463bf1
kim junghwan <kimjunghwan@kimui-MacBookPro.local>
2020-04-10 12:20:10 +0900

    router test

--- src/components/Router.js
+++ src/components/Router.js
@@ -7,13 +7,15 @@ import Fillgi from "pages/Fillgi";
 
 export default () => (
   <Router>
-    <ScrollToTop>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/resume" component={Resume} />
-        <Route path="/Fillgi" component={Fillgi} />
-        <Redirect from="*" to="/" />
-      </Switch>
-    </ScrollToTop>
+    <>
+      <ScrollToTop>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/resume" component={Resume} />
+          <Route path="/Fillgi" component={Fillgi} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </ScrollToTop>
+    </>
   </Router>
 );
