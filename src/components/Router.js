2188d761d5490913d30a5ec30f83a1d5a7efbe1a
kim junghwan <kimjunghwan@kimui-MacBookPro.local>
2020-04-10 11:52:15 +0900

    change hashrouter

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import ScrollToTop from './ScrollToTop';
 import Home from "pages/Home";
 import Resume from "pages/Resume";
