a95af1ff5da97495f2ad29dacffc798f81f94bae
kim junghwan <kimjunghwan@kimui-MacBookPro.local>
2020-04-10 11:48:38 +0900

    detail remove

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import ScrollToTop from './ScrollToTop';
 import Home from "pages/Home";
-import Detail from "pages/Detail";
 import Resume from "pages/Resume";
 import Fillgi from "pages/Fillgi";
 
@@ -14,7 +13,6 @@ export default () => (
           <Route path="/" exact component={Home} />
           <Route path="/resume" exact component={Resume} />
           <Route path="/Fillgi" exact component={Fillgi} />
-          <Route path="/:id" component={Detail} />
           <Redirect from="*" to="/" />
         </Switch>
       </ScrollToTop>
